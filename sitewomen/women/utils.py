edbed41acbaa1333e6b0950f8cc70abfd1fff7cc
Dima <ansfer91@gmail.com>
2026-05-28 03:44:54 +0300

    complete classViews

--- sitewomen/women/utils.py
+++ sitewomen/women/utils.py
@@ -5,7 +5,7 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
 
 
 class DataMixin:
-    paginate_by = 3
+    paginate_by = 5
     title_page = None
     extra_context = {}
     cat_selected = None
